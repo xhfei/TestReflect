929be90dec78686180c6b7152fcaf7ea8f1a2b2c
xhfei <xhfei1983@gmail.com>
2014-02-13 14:42:08 +0800

    modify GetImageService

--- src/reflect/image/GetImgService.java
+++ src/reflect/image/GetImgService.java
@@ -3,10 +3,13 @@ package reflect.image;
 import reflect.image.annotation.GetPic;
 import reflect.image.annotation.PicService;
 
-@PicService("com.hongfei.xu")
+@PicService("com.fly.test")
 public interface GetImgService {
 
 	@GetPic(value="renren",type="get")
 	public void downloadImage(String url,String localFile);
+	
+	@GetPic(value="renren",type="get")
+	public void downloadImage2(String url,String localFile);
 
 }
